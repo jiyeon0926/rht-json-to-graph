9f115fb92ac9cbdbf226f5ff1034714cff7d3f7e
jiyeon0926 <yeoni9094@naver.com>
2025-05-27 11:49:25 +0900

    fix: ResDto 수정

---
+++ src/main/java/com/example/graph/dto/AnalyticDto.java
@@ -0,0 +1,24 @@
+package com.example.graph.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@RequiredArgsConstructor
+public class AnalyticDto {
+
+    private final Long id; // 고유 식별자
+    private final String contentId; // 영상 아이디
+    private final String videoTitle; // 영상 제목
+    private final LocalDateTime publishTime; // 영상 게시일
+    private final String videoLength; // 영상 길이
+    private final int validViews; // 유효 조회수
+    private final int views; // 조회수
+    private final BigDecimal watchTimeHours; // 시청 시간
+    private final int subscribers; // 구독자 수
+    private final int impressions; // 노출 수
+    private final BigDecimal impressionsClickRate; // 노출 클릭률
+}
--- src/main/java/com/example/graph/dto/AnalyticResDto.java
+++ src/main/java/com/example/graph/dto/AnalyticResDto.java
@@ -4,12 +4,18 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 
 @Getter
 @RequiredArgsConstructor
 public class AnalyticResDto {
 
-    private final Long id;
-    private final int subscribers;
-    private final BigDecimal impressionClickRate;
+    private final String channelName; // 채널명
+    private final LocalDate collectedAt; // 수집일
+    private final int totalValidViews; // 총 유효 조회수
+    private final int totalViews; // 총 조회수
+    private final int totalSubscribers; // 신규 구독자 수의 누적 합계 (!= 채널 구독자 수)
+    private final BigDecimal totalWatchTimeHours; // 총 시청 시간
+    private final List<AnalyticDto> videos;
 }
