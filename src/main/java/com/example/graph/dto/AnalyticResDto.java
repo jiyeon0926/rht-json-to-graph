3b2ef0f9a23e3b6f0742357c1a6f322d9ecb8549
jiyeon0926 <yeoni9094@naver.com>
2025-05-27 14:09:33 +0900

    fix: DTO 수정
    - @QueryProjection 적용

--- src/main/java/com/example/graph/dto/AnalyticResDto.java
+++ src/main/java/com/example/graph/dto/AnalyticResDto.java
@@ -1,14 +1,13 @@
 package com.example.graph.dto;
 
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
 @Getter
-@RequiredArgsConstructor
 public class AnalyticResDto {
 
     private final String channelName; // 채널명
@@ -18,4 +17,17 @@ public class AnalyticResDto {
     private final int totalSubscribers; // 신규 구독자 수의 누적 합계 (!= 채널 구독자 수)
     private final BigDecimal totalWatchTimeHours; // 총 시청 시간
     private final List<AnalyticDto> videos;
+
+    @QueryProjection
+    public AnalyticResDto(String channelName, LocalDate collectedAt, int totalValidViews,
+                          int totalViews, int totalSubscribers, BigDecimal totalWatchTimeHours,
+                          List<AnalyticDto> videos) {
+        this.channelName = channelName;
+        this.collectedAt = collectedAt;
+        this.totalValidViews = totalValidViews;
+        this.totalViews = totalViews;
+        this.totalSubscribers = totalSubscribers;
+        this.totalWatchTimeHours = totalWatchTimeHours;
+        this.videos = videos;
+    }
 }
