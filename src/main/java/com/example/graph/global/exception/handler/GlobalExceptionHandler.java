5c2ef506c84b1c0bac9a148cb58e0bc3cb36177f
jiyeon0926 <yeoni9094@naver.com>
2025-06-02 10:44:12 +0900

    feat: ValidationException 추가

--- src/main/java/com/example/graph/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/graph/global/exception/handler/GlobalExceptionHandler.java
@@ -8,14 +8,41 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.nio.file.AccessDeniedException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(HandlerMethodValidationException.class)
+    protected ResponseEntity<String> handleMethodValidationExceptions(HandlerMethodValidationException e) {
+        String message = e.getParameterValidationResults()
+                .getFirst()
+                .getResolvableErrors()
+                .getFirst()
+                .getDefaultMessage();
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(message);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationExceptions(MethodArgumentNotValidException e) {
+        String message = e.getBindingResult()
+                .getAllErrors()
+                .getFirst()
+                .getDefaultMessage();
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(message);
+    }
 
     @ExceptionHandler(ResponseStatusException.class)
     protected ResponseEntity<String> handleResponseStatusExceptions(ResponseStatusException e) {
